a11db8421537cae9977a4d3b968daaacf230d9ba
Tom Dörr <tom.doerr@tum.de>
2021-01-06 21:13:20 +0100

    Add log output

--- view_best_image.sh
+++ view_best_image.sh
@@ -2,6 +2,8 @@
 
 while true
 do
-    ln -fs $(sort -n results/Camera | tail -n1 | awk '{print $2}') best_image
+    best_image_log="$(sort -n results/Camera | tail -n1)"
+    echo $best_image_log
+    ln -fs $(echo $best_image_log | awk '{print $2}') best_image
     sleep 10
 done
