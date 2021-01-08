9c46a9af8e22fb382d3c0ec992c9463e30099238
Tom Dörr <tom.doerr@tum.de>
2021-01-08 02:22:36 +0100

    Add file

---
+++ sort_to_dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+log_file=$1
+top_num=$(sort -n $log_file | tail -n1 | awk '{print $1}')
+echo top_num: $top_num
+
+log_file_dir_sorted="$log_file"_sorted
+for i in {0..6}
+do
+    mkdir -p $log_file_dir_sorted/$i
+    for e in $(cat $log_file  | awk '/'"$i"'\./ {print $2}')
+    do
+        ln -s $e $log_file_dir_sorted/$i/
+    done
+done
