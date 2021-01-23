5605b8a79e3d2a0b86b850ca88678ea8d30da2a9
Tom Dörr <tom.doerr@tum.de>
2021-01-23 02:12:54 +0100

    Add support png

--- rank.sh
+++ rank.sh
@@ -5,6 +5,6 @@ set -x
 base_path="$1"
 filter="$2"
 
-eval 'find $base_path/* | grep "\.jpg" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a results/"$base_path"
+eval 'find $base_path/* | egrep "\.(jpg|png)" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a results/"$base_path"
 ./sort_to_dir.sh $base_path
 
