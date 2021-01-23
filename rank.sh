2b4d225a97bbf086ab4e35f3d1391fd8d4093b64
Tom Dörr <tom.doerr@tum.de>
2021-01-23 02:07:49 +0100

    Update file

--- rank.sh
+++ rank.sh
@@ -6,4 +6,5 @@ base_path="$1"
 filter="$2"
 
 eval 'find $base_path/* | grep "\.jpg" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a results/"$base_path"
+./sort_to_dir.sh $base_path
 
