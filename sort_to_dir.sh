79c78e7c33831578da43aa736c80c3039ce5073b
Tom Dörr <tom.doerr@tum.de>
2021-01-23 02:07:56 +0100

    Update file

--- sort_to_dir.sh
+++ sort_to_dir.sh
@@ -7,11 +7,11 @@ top_num=$(sort -n $log_file | tail -n1 | awk '{print $1}')
 echo top_num: $top_num
 
 log_file_dir_sorted="$log_file"_sorted
-for i in {0..6}
+for i in {0..8}
 do
     mkdir -p $log_file_dir_sorted/$i
     for e in $(cat $log_file  | awk '/ '"$i"'\./ {print $2}')
     do
-        ln -s $e $log_file_dir_sorted/$i/
+        ln -s ../../../$e $log_file_dir_sorted/$i/
     done
 done
