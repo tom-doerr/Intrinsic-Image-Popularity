7ff45c0805a38026e11bd4c0402c5dfae802bb2e
Tom Dörr <tom.doerr@tum.de>
2021-01-24 01:58:18 +0100

    Fix bug

--- camera_pipeline.sh
+++ camera_pipeline.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-./rank.sh Camera
+set -x 
+
+./rank.sh Camera 2021
 cd /home/tom/git/Intrinsic-Image-Popularity/results/Camera_sorted
 
 for e in {4..7}
 do
-        cp -rL $e/ ~/syncthing/results/$e
+        rating_class_path=~/syncthing/results/$e/
+        mkdir -p $rating_class_path
+        cp -rL $e/* $rating_class_path
 done
