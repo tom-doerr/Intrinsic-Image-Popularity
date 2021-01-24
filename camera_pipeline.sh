0c8f3c1f1279f63cab0eaefad797993d9e09956d
Tom Dörr <tom.doerr@tum.de>
2021-01-24 02:18:06 +0100

    Update file

--- camera_pipeline.sh
+++ camera_pipeline.sh
@@ -2,10 +2,12 @@
 
 set -x 
 
+source venv/bin/activate
+
 ./rank.sh Camera 2021
 cd /home/tom/git/Intrinsic-Image-Popularity/results/Camera_sorted
 
-for e in {4..7}
+for e in {2..7}
 do
         rating_class_path=~/syncthing/results/$e/
         mkdir -p $rating_class_path
