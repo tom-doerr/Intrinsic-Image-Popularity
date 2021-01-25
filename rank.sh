515ee363c329b274ece941671b131e06ab88bcd9
Tom Dörr <tom.doerr@tum.de>
2021-01-25 04:49:23 +0100

    Add ckeck files new

--- rank.sh
+++ rank.sh
@@ -5,6 +5,21 @@ set -x
 base_path="$1"
 filter="$2"
 
-eval 'find $base_path/* | egrep "\.(jpg|png)" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a results/"$base_path"
-./sort_to_dir.sh $base_path
+files_without_score=""
+for e in $base_path/$filter*\.jpg
+do
+        if ! grep $e scores/Camera 
+        then
+                echo e: $e                                         
+                files_without_score="$files_without_score\n$e"
+        fi
+done
+echo "$files_without_score"
+printf "$files_without_score"
+
+printf "$files_without_score" | parallel -j1 python3 test.py --image_path  | tee -a scores/$base_path
+#printf "$files_without_score" | grep "\.jpg" | parallel -j1 python3 test.py --image_path  | tee -a scores/$base_path
+#eval "echo $files_without_score"' | grep "\.jpg" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a scores/"$base_path"
+#eval 'find $base_path/* | grep "\.jpg" | grep "$filter" | parallel -j1 python3 test.py --image_path ' | tee -a scores/"$base_path"
+#./sort_to_dir.sh scores/$base_path
 
