f7c636bb9c8b89f5f645b0d50cdd3428aadd1b34
Tom Dörr <tom.doerr@tum.de>
2021-01-25 05:44:00 +0100

    Change sync dirs

--- camera_pipeline.sh
+++ camera_pipeline.sh
@@ -8,7 +8,7 @@ export CUDA_VISIBLE_DEVICES=1
 ./rank.sh Camera
 cd /home/tom/git/Intrinsic-Image-Popularity/results/Camera_sorted
 
-for e in {2..7}
+for e in {3..7}
 do
         rating_class_path=~/syncthing/results/$e/
         mkdir -p $rating_class_path
